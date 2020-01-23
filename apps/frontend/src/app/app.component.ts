4b53411779d71fd7ee65dca1a10f7d31b70c2aa8
maxeber <maxhebert13@gmail.com>
2020-01-23 09:31:17 -0500

    Initial setup

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Message } from '@ubisoft-dna-tech-project/api-interfaces';
+import { Component } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { Message } from '@ubisoft-dna-tech-project/api-interfaces'
 
 @Component({
   selector: 'ubisoft-dna-tech-project-root',
@@ -8,6 +8,6 @@ import { Message } from '@ubisoft-dna-tech-project/api-interfaces';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
+  hello$ = this.http.get<Message>('/api/hello')
   constructor(private http: HttpClient) {}
 }
