9dc3e1b8abc4ed8f7694e471feddc066287d39f9
maxeber <maxhebert13@gmail.com>
2020-01-23 09:01:27 -0500

    initial commit

---
+++ apps/frontend/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Message } from '@ubisoft-dna-tech-project/api-interfaces';
+
+@Component({
+  selector: 'ubisoft-dna-tech-project-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  hello$ = this.http.get<Message>('/api/hello');
+  constructor(private http: HttpClient) {}
+}
