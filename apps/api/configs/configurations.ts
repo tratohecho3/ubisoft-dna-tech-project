4cecec667ecd273009bf00a52920a86e344f1d81
maxeber <maxhebert13@gmail.com>
2020-01-23 13:25:08 -0500

    remove configs interfaces

--- apps/api/configs/configurations.ts
+++ apps/api/configs/configurations.ts
@@ -1,11 +1,4 @@
-export interface IConfigurations {
-  twitch: {
-    clientId: string
-    token: string
-  }
-}
-
-export const configurations = (): IConfigurations => ({
+export default () => ({
   twitch: {
     clientId: process.env.TWITCH_CLIENT_ID,
     token: process.env.TWITCH_TOKEN
--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { join } from 'path'
 
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { configurations } from '../../configs/configurations'
+import configurations from '../../configs/configurations'
 
 @Module({
   imports: [
