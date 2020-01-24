4bfa0f5d8e7a93ce2fdbc33af447e4d3c5d35a85
maxeber <maxhebert13@gmail.com>
2020-01-24 13:54:30 -0500

    add barchart component skeleton

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { NgModule } from '@angular/core'
 import { AppComponent } from './app.component'
 import { HttpClientModule } from '@angular/common/http'
 import { PubnubService } from './services/pubnub.service'
+import { BarchartComponent } from './components/barchart/barchart.component'
 import { PubNubAngular } from 'pubnub-angular2'
 
 @NgModule({
+    BarchartComponent
   providers: [PubnubService, PubNubAngular],
   bootstrap: [AppComponent]
 })
---
+++ apps/frontend/src/app/components/barchart/barchart.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'ubisoft-dna-tech-project-barchart',
+  templateUrl: './barchart.component.html',
+  styleUrls: ['./barchart.component.css']
+})
+export class BarchartComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
