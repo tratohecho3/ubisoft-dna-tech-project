5f911cc511c5ee6f951aad294bf1b940901498ae
maxeber <maxhebert13@gmail.com>
2020-01-24 00:37:23 -0500

    enable cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
   const port = process.env.PORT || 3333
 
   app.setGlobalPrefix(globalPrefix)
+  app.enableCors()
 
   await app.listen(port, () => {
     console.log('Listening at http://localhost:' + port + '/' + globalPrefix)
